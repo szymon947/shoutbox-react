677a6d68d1e7513a15767cc16cdcd409d8b8aa05
szymon szymon <rowerrower30@gmail.com>
2026-04-18 13:01:29 +0200

    JD67

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { registerSW } from 'virtual:pwa-register'
+import { registerSW } from 'vite-plugin-pwa/register'
 
 registerSW({ immediate: true })
 
